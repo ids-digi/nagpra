0386a9e078c1d8d06804ec4210e2e59f26633aba
carsonology <carson.terbush@gmail.com>
2023-02-03 19:25:36 -0500

    add bubble map

--- js/d3.js
+++ js/d3.js
@@ -1,159 +1,198 @@
 'use strict';
 
-// Copyright 2022 Observable, Inc.
-// Released under the ISC license.
-// https://observablehq.com/@d3/bubble-map
-function BubbleMap(data, {
-    position = d => d, // given d in data, returns the [longitude, latitude]
-    value = () => undefined, // given d in data, returns the quantitative value
-    title, // given a datum d, returns the hover text
-    scale = d3.scaleSqrt, // type of radius scale
-    domain, // [0, max] values; input of radius scale; must start at zero
-    maxRadius = 40, // maximum radius of bubbles
-    width = 640, // outer width, in pixels
-    height, // outer height, in pixels
-    projection, // a D3 projection; null for pre-projected geometry
-    features, // a GeoJSON feature collection for the background
-    borders, // a GeoJSON object for stroking borders
-    outline = projection && projection.rotate ? { type: "Sphere" } : null, // a GeoJSON object for the background
-    backgroundFill = "#e0e0e0", // fill color for background
-    backgroundStroke = "white", // stroke color for borders
-    backgroundStrokeWidth, // stroke width for borders
-    backgroundStrokeOpacity, // stroke width for borders
-    backgroundStrokeLinecap = "round", // stroke line cap for borders
-    backgroundStrokeLinejoin = "round", // stroke line join for borders
-    fill = "brown", // fill color for bubbles
-    fillOpacity = 0.5, // fill opacity for bubbles
-    stroke = "white", // stroke color for bubbles
-    strokeWidth = 0.5, // stroke width for bubbles
-    strokeOpacity, // stroke opacity for bubbles
-    legendX = width - maxRadius - 10,
-    legendY = height - 10,
-} = {}) {
-    // Compute values.
-    const I = d3.map(data, (_, i) => i);
-    const V = d3.map(data, value).map(d => d == null ? NaN : +d);
-    const P = d3.map(data, position);
-    const T = title == null ? null : d3.map(data, title);
-
-    // Compute default domains.
-    if (domain === undefined) domain = [0, d3.max(V)];
-
-    // Construct scales.
-    const radius = scale(domain, [0, maxRadius]);
-
-    // Compute the default height. If an outline object is specified, scale the projection to fit
-    // the width, and then compute the corresponding height.
-    if (height === undefined) {
-        if (outline === undefined) {
-            height = 400;
-        } else {
-            const [[x0, y0], [x1, y1]] = d3.geoPath(projection.fitWidth(width, outline)).bounds(outline);
-            const dy = Math.ceil(y1 - y0), l = Math.min(Math.ceil(x1 - x0), dy);
-            projection.scale(projection.scale() * (l - 1) / l).precision(0.2);
-            height = dy;
-        }
-    }
-
-    // Construct a path generator.
-    const path = d3.geoPath(projection);
-
-    const svg = d3.create("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [0, 0, width, height])
-        .attr("style", "width: 100%; height: auto; height: intrinsic;");
-
-    if (outline != null) svg.append("path")
-        .attr("fill", "white")
-        .attr("stroke", "currentColor")
-        .attr("d", path(outline));
-
-    svg.append("path")
-        .datum(features)
-        .attr("fill", backgroundFill)
-        .attr("d", path);
-
-    if (borders != null) svg.append("path")
-        .attr("pointer-events", "none")
-        .attr("fill", "none")
-        .attr("stroke", backgroundStroke)
-        .attr("stroke-linecap", backgroundStrokeLinecap)
-        .attr("stroke-linejoin", backgroundStrokeLinejoin)
-        .attr("stroke-width", backgroundStrokeWidth)
-        .attr("stroke-opacity", backgroundStrokeOpacity)
-        .attr("d", path(borders));
-
-    const legend = svg.append("g")
-        .attr("fill", "#777")
-        .attr("transform", `translate(${legendX},${legendY})`)
-        .attr("text-anchor", "middle")
-        .style("font", "10px sans-serif")
-        .selectAll("g")
-        .data(radius.ticks(4).slice(1))
-        .join("g");
-
-    legend.append("circle")
-        .attr("fill", "none")
-        .attr("stroke", "#ccc")
-        .attr("cy", d => -radius(d))
-        .attr("r", radius);
-
-    legend.append("text")
-        .attr("y", d => -2 * radius(d))
-        .attr("dy", "1.3em")
-        .text(radius.tickFormat(4, "s"));
-
-    svg.append("g")
-        .attr("fill", fill)
-        .attr("fill-opacity", fillOpacity)
-        .attr("stroke", stroke)
-        .attr("stroke-width", strokeWidth)
-        .attr("stroke-opacity", strokeOpacity)
-        .selectAll("circle")
-        .data(d3.range(data.length)
-            .filter(i => { P[i] })
-            .sort((i, j) => d3.descending(V[i], V[j])))
-        .join("circle")
-        .attr("transform", projection == null
-            ? i => `translate(${P[i]})`
-            : i => `translate(${projection(P[i])})`)
-        .attr("r", i => radius(V[i]))
-        .call(T ? circle => circle.append("title").text(i => T[i]) : () => { });
-
-
-    return Object.assign(svg.node(), { scales: { radius } });
-}
-
-const centroid = () => {
-    const path = d3.geoPath();
-    return feature => path.centroid(feature);
-}
+/*
+    GET DATA
+*/
 
 const us = albers
-const nation = topojson.feature(us, us.objects.nation)
-const statemap = new Map(topojson.feature(us, us.objects.states).features.map(d => [d.id, d]))
-const countymap = new Map(topojson.feature(us, us.objects.counties).features.map(d => [d.id, d]))
-const statemesh = topojson.mesh(us, us.objects.states, (a, b) => a !== b)
 
-const chart = BubbleMap(population, {
-    value: ([population]) => +population,
-    position([, , stateid, countyid]) {
-        // console.log(countyid)
-        const county = countymap.get(stateid + countyid);
-        return county && centroid(county);
-    },
-    title([population, , stateid, countyid]) {
-        // console.log(population)
-        const state = statemap.get(stateid);
-        const county = countymap.get(stateid + countyid);
-        return `${county?.properties.name}, ${state?.properties.name}\n${(+population).toLocaleString("en")}`;
-    },
-    features: nation,
-    borders: statemesh,
-    width: 975,
-    height: 610
-})
+const drawMap = (countyData, stateData) => {
 
-document.querySelector('#bubble-map').insertAdjacentElement('afterbegin', chart)
+    // var statemap = new Map(topojson.feature(us.objects.states).features.map(d => [d.id, d]))
+
+    // set width/height (aspect ratio) for map
+    var width = 975,
+        height = 610;
+    // create path
+    var path = d3.geoPath()
+        .projection(null);
+    // grab div by id, inject svg
+    var svg = d3.select("#bubble-map").append("svg")
+        .attr("preserveAspectRatio", "xMidYMid")
+        .attr("viewBox", "0 0 " + width + " " + height);
+    // define bubble radius scale
+    var radius = d3.scaleSqrt()
+        .domain([0, 400])
+        .range([0, 25])
+    // choropleth color scale
+
+    // const colorScale = d3.scaleLinear()
+    //     .domain([0, 3380])
+    //     .range(["#000", "#fff"])
+    //     .interpolate(d3.interpolateHcl)
+
+    const colorScale = d3.scaleLinear()
+        .domain([0, 50, 100, 3380])
+        // .domain(d3.quantile)
+        .range(d3.schemeBlues[5])
+
+    const choroLegend = Legend(d3.scaleSequentialQuantile(d3.range(100).map(() => Math.random() ** 2), d3.interpolateBlues), {
+        title: "Quantile",
+        tickFormat: ".2f"
+    })
+
+    // console.log(choroLegend)
+
+    // svg.append(choroLegend)
+
+    // d3.select("#bubble-map").append(choroLegend)
+
+    // console.log(choroLegend)
+    // tooltip
+    var tooltip = d3.select("body")
+        .append("div")
+        .style("position", "absolute")
+        .attr('class', 'tooltip')
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+
+    // add mni data to topojson data
+    topojson.feature(us, us.objects.counties).features.map((county) => {
+        const mni = countyData.find((item) => {
+            if ((item['CountyFIPS']) === county.id.toString()) {
+                return (item['CountyFIPS']) === county.id.toString()
+            } else {
+                return 0
+            }
+        })
+        county.properties['mni'] = mni ? parseInt(mni['MNI']) : 0
+        county.properties['state'] = mni ? mni['StateName'] : 'null'
+        return county
+    })
+    // add state totals
+    topojson.feature(us, us.objects.states).features.map((state) => {
+        const mni = stateData.find((item) => {
+            if ((item['StateFIPS']) === state.id.toString()) {
+                return (item['StateFIPS']) === state.id.toString()
+            } else {
+                return 0
+            }
+        })
+        state.properties['mni'] = mni ? parseInt(mni['MNI']) : 0
+        return state
+    })
+    svg.append("defs").append("pattern")
+        .attr('id', 'diagonal-stripe')
+        .attr("width", 2.5)
+        .attr("height", 5)
+        .attr('patternUnits', "userSpaceOnUse")
+        .attr('patternTransform', "rotate(-45)")
+        .append('path')
+        // .attr('fill', 'darkgray')
+        .attr('stroke', 'rgba(0,0,0,.35)')
+        .attr('stroke-width', '.5')
+        .attr('d', 'M -1,2 l 6,0')
+
+    const drawStates = (condition, fill, className = "") => {
+        // condition: "zero", "nonzero"
+        // fill: explicit or "color" for colorscale
+        return svg.append("g")
+            .selectAll("path")
+            .data(topojson.feature(us, us.objects.states).features.filter((state) => condition == "zero" ? state.properties.mni === 0 : state.properties.mni !== 0))
+            .enter()
+            .append("path")
+            .attr('d', d3.geoPath().projection(null))
+            .attr("class", className)
+            .attr('fill', fill == "color" ? d => colorScale(d.properties.mni) : fill)
+    }
+
+    // null states
+    drawStates("zero", "rgba(0,0,0,.04")
+    drawStates("zero", "url(#diagonal-stripe)")
+
+    // choropleth states
+    drawStates("nonzero", "color", "state").on("mouseover", (d, i) => {
+        console.log(d.screenY)
+        console.log(d)
+        console.log(i)
+        tooltip.style("opacity", 1)
+            .html(`<p><strong>${i.properties.name}:</strong> IU still has remains of at least <strong>${i.properties.mni}</strong> individuals</p>`)
+            .style("visibility", 'visible')
+            .style("left", (d.pageX + 10) + "px")
+            .style("top", (d.pageY + 10) + "px")
+    }).on("mousemove", (d, i) => {
+        tooltip.style("opacity", 1)
+            .style("left", (d.pageX + 10) + "px")
+            .style("top", (d.pageY + 10) + "px")
+    }).on("mouseout", (d, i) => {
+        tooltip.style("opacity", 0)
+            .style("visibility", 'hidden')
+    })
+
+    // states outline 
+    svg.append("path")
+        .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+        .attr("class", "border border--state")
+        .attr("d", path)
+        .attr("stroke", "white")
+        .attr("stroke-width", "1px")
+
+    // map the bubbles
+    svg.append("g")
+        .attr("class", "bubble")
+        .selectAll("circle")
+        .data(topojson.feature(us, us.objects.counties).features
+            // sort smallest to largest to smaller bubbles aren't covered
+            .sort((a, b) => b.properties.mni - a.properties.mni)
+        )
+        .enter().append("circle")
+        .attr("transform", (d) => `translate(${path.centroid(d)})`)
+        .attr("r", (d) => radius(d.properties.mni))
+        .on("mouseover", (d, i) => {
+            console.log(d.screenY)
+            console.log(d)
+            console.log(i)
+            tooltip.style("opacity", 1)
+                .html(`<p><strong>${i.properties.name} County, ${i.properties.state}:</strong> IU still has remains of at least <strong>${i.properties.mni}</strong> individuals</p>`)
+                .style("visibility", 'visible')
+                .style("left", (d.pageX + 10) + "px")
+                .style("top", (d.pageY + 10) + "px")
+        }).on("mousemove", (d, i) => {
+            tooltip.style("opacity", 1)
+                .style("left", (d.pageX + 10) + "px")
+                .style("top", (d.pageY + 10) + "px")
+        }).on("mouseout", (d, i) => {
+            tooltip.style("opacity", 0)
+                .style("visibility", 'hidden')
+        })
+
+    // add legend for circles
+    var legend = svg.append("g")
+        .attr("class", "legend")
+        .attr("transform", "translate(" + (width - 110) + "," + (height - 130) + ")")
+        .selectAll("g")
+        .data([50, 200, 400])
+        .enter().append("g");
+    legend.append("circle")
+        .attr("cy", function (d) { return -radius(d); })
+        .attr("r", radius);
+    legend.append("text")
+        .attr("y", function (d) { return -2 * radius(d); })
+        .attr("dy", "1.3em")
+        .text(d3.format(".1s"));
+
+}
+
+
+// asynchronously fetch county + state aggregate data
+const asyncCounties = async () => {
+    return d3.csv('./files/inventory_clean.csv')
+}
+const asyncStates = async () => {
+    return d3.csv('./files/state_df.csv')
+}
+    // once promises return, run the map drawing function
+    ; (async () => {
+        drawMap(await asyncCounties(), await asyncStates())
+    })()
 
